ccd9139bd4fcfb319d909d885ca674a3dcdd8ed7
JhonFM <jfranciaminaya@gmail.com>
2025-04-21 01:03:05 -0500

    Se agrega la tabla foránea marcas

--- config/scriptdb.sql
+++ config/scriptdb.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE crudnode;
 USE crudnode;
 
+/*
 CREATE TABLE vehiculos
 (
 	id			INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,6 +9,30 @@ CREATE TABLE vehiculos
     marca 		ENUM('Kia','Toyota','Nissan','Hyundai','Javal') NOT NULL,
 	color 		VARCHAR(40) 		NOT NULL
 )ENGINE = INNODB;
+*/
+
+CREATE TABLE marcas
+(
+	id 			INT AUTO_INCREMENT PRIMARY KEY,
+    marca 		VARCHAR(40) 		NOT NULL
+)ENGINE = INNODB;
+
+CREATE TABLE vehiculos
+(
+	id			INT AUTO_INCREMENT PRIMARY KEY,
+    tipo 		VARCHAR(40) 		NOT NULL,
+    idmarca 	INT 				NOT NULL,
+	color 		VARCHAR(40) 		NOT NULL,
+    CONSTRAINT fk_idmarca_veh FOREIGN KEY (idmarca) REFERENCES marcas (id)
+)ENGINE = INNODB;
+
+INSERT INTO marcas (marca) VALUES 
+	(''),
+    (''),
+    (''),
+    (''),
+    (''),
+    ('');
 
 INSERT INTO vehiculos (tipo, marca, color) VALUES
 	('Hatchback', 'Kia', 'Blanco'),
